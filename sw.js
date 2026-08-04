d9f3dfa42da4ca59ad2b2958c10aff5fef2828ee
wowmobilya <www.wowmobilya@gmail.com>
2026-08-04 17:51:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ⚠️ غيّر هذا الرقم (v2, v3, v4...) في كل مرة تقوم فيها بتعديل التطبيق وتريد نشره
-const CACHE_NAME = 'wow-furniture-v2 '; 
+const CACHE_NAME = 'wow-furniture-v3 '; 
 
 const urlsToCache = [
   './',
